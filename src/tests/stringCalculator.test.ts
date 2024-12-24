6788fbcad77ad2d0ed5c81f7a4ac0924dd6550e2
Rahul555-droid <rahulmohta2000@gmail.com>
2024-12-24 14:23:27 +0530

    Add support for new lines as delimiters

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -18,3 +18,9 @@ test("returns the sum for multiple comma-separated numbers", () => {
   expect(add("1,2,3,4")).toBe(10);
   expect(add("10,20,30,40")).toBe(100);
 });
+
+
+test("handles new lines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+  expect(add("10\n20,30")).toBe(60);
+});
